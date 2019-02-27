361f84a5c388742f5d17f6dda5f268ac1c643ccd
Alexander Samoylov <smlwmy@gmail.com>
2019-02-27 20:11:48 +0500

    create template to start

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  .container
+    v-app#inspire
+      .text-xs-center
+        v-menu(offset-y='')
+          v-btn(slot='activator', color='primary', dark='')
+            | Dropdown
+          v-list
+            v-list-tile(v-for='(item, index) in items', :key='index', @click='')
+              v-list-tile-title {{ item.title }}
+
+</template>
+
+<script>
+export default {
+  data: () => ({
+    items: [
+      { title: 'Click Me' },
+      { title: 'Click Me' },
+      { title: 'Click Me' },
+      { title: 'Click Me 2' }
+    ]
+  })
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>

83935841536c12d9ea082a543280d576ea1d7ef3
Alexander Samoylov <smlwmy@gmail.com>
2019-03-04 17:37:04 +0500

    add responsive menu

--- src/layouts/main.vue
+++ src/layouts/main.vue
@@ -17,18 +17,32 @@
           )
         span Самойлов Александр
       .user-profiles
-        v-divider
         v-list
-          v-list-tile(v-for='item in items', :key='item.title', avatar='', @click='')
-            router-link.profile-links(
-              :to="`profile/${item.id}`"
-            )
-              v-list-tile-action
-                v-icon(v-if='item.icon', color='pink') star
-              v-list-tile-content
-                v-list-tile-title(v-text='item.title')
-              v-list-tile-avatar
-                img(:src='item.avatar')
+          v-list-group(prepend-icon='account_circle', value='true')
+            template(v-slot:activator='')
+              v-list-tile
+                v-list-tile-title Профили
+            v-list-tile(v-for='item in items', :key='item.title', avatar='', @click='')
+              router-link.profile-links(
+                :to="`profile/${item.id}`"
+              )
+                v-list-tile-action
+                  v-icon(v-if='item.icon', color='pink') star
+                v-list-tile-content
+                  v-list-tile-title(v-text='item.title')
+                v-list-tile-avatar
+                  img(:src='item.avatar')
+          v-list-group.hidden-lg-and-up(prepend-icon='account_circle', value='true')
+            template(v-slot:activator='')
+              v-list-tile
+                v-list-tile-title Действия
+            v-list-tile(v-for='(item, i) in menuItems', :key='i', @click='')
+              router-link.features-link(
+                :to="`${item.to}`"
+              )
+                v-layout(align-start='', justify-space-between='', row='')
+                  span {{ item.title }}
+                  v-icon {{ item.icon }}
         v-divider
         v-list
           v-list-tile
@@ -40,13 +54,14 @@
       v-layout.hidden-lg-and-up(align-center='', justify-space-between='', row='', fill-height='')
         v-icon(@click.stop='drawer = !drawer') menu
         .logo() Logo
-      router-link.features-link.hidden-md-and-down(
-        v-for='(item, index) in menuItems',
-        :key='index',
-        :to="`${item.to}`")
-        v-layout(align-center justify-start row fill-height)
-          v-icon {{ item.icon }}
-          span {{ item.title }}
+      v-layout(align-center='', justify-start='', row='', fill-height='')
+        router-link.features-link.hidden-md-and-down(
+          v-for='(item, index) in menuItems',
+          :key='index',
+          :to="`${item.to}`")
+          v-layout(align-center justify-start row fill-height)
+            v-icon {{ item.icon }}
+            span {{ item.title }}
     // Content wrapper
     v-content
       v-container(fluid)
@@ -61,7 +76,7 @@ export default {
       drawer: null,
       tile: false,
       items: [
-        { id: 1, title: 'Jason Oner', avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg', icon: true },
+        { id: 1, title: 'Jason Oner', avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg' },
         { id: 2, title: 'Travis Howard', avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg' },
         { id: 3, title: 'Ali Connors', avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg' },
         { id: 4, title: 'Ali2d Connors', avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg' },
@@ -71,6 +86,12 @@ export default {
         {title: 'Добавить профайл', to: '/add', icon: 'add_circle'},
         {title: 'Мои профайлы', to: '/dashboard', icon: 'list_alt'},
         {title: 'Календарь', to: '/calendar', icon: 'calendar_today'}
+      ],
+      cruds: [
+        ['Create', 'add'],
+        ['Read', 'insert_drive_file'],
+        ['Update', 'update'],
+        ['Delete', 'delete']
       ]
     }
   }
@@ -94,6 +115,9 @@ export default {
   text-decoration none
   margin 0 15px 0 0
   padding 5px
+  width 100%
+  @media screen and (min-width: 1264px)
+    width auto
   &:last-child
     margin 0
   &:hover

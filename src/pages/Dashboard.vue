134c5f50c42c324d58fc851628276cd4ad20095f
Alexander Samoylov <smlwmy@gmail.com>
2019-03-05 11:54:48 +0500

    change cards

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -7,19 +7,13 @@
       )
         div(v-for="(profile, index) in items" :key="index")
           v-card.item
-            v-img(:src="`${profile.avatar}`" aspect-ratio='1.5')
-            v-card-title(primary-title)
+            v-img(:src="`${profile.avatar}`", height='200px')
+            v-card-title(primary-title='')
               div
-                h4.card-headline.mb-0 {{ profile.title }}
-                h5.profile-info.mb-0 20 лет
+                .headline {{ profile.title }}
+                span.grey--text {{ profile.age }} лет. {{ profile.profession }}
             v-card-actions
-              v-layout(align-start justify-space-between row fill-height)
-                router-link.profile-links(
-                  :to="`profile/${profile.id}`"
-                )
-                  v-btn(flat='', color='orange') Смотреть
-                v-btn(flat='', color='red')
-                  v-icon(red) delete_forever
+              v-btn(flat='') Смотреть
 </template>
 
 <script>
@@ -29,11 +23,10 @@ export default {
     return {
       tile: false,
       items: [
-        { id: 1, title: 'Jason Oner', avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg', icon: true },
-        { id: 2, title: 'Travis Howard', avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg' },
-        { id: 3, title: 'Ali Connors', avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg' },
-        { id: 4, title: 'Ali2d Connors', avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg' },
-        { id: 4, title: 'Ali2d Connor3s', avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg' }
+        { id: 1, title: 'Jason Oner', avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg', age: '20', profession: 'Студент', icon: true },
+        { id: 2, title: 'Travis Howard', avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg', age: '21', profession: 'Врач' },
+        { id: 3, title: 'Ali Connors', avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg', age: '20', profession: 'Слесарь' },
+        { id: 4, title: 'Ali2d Connors', avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg', age: '18', profession: 'Педагог' }
       ]
     }
   },

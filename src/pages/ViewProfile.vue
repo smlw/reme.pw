ccf42205e206a07b853baf0ed9559737455616f4
Alexander Samoylov <smlwmy@gmail.com>
2019-03-14 16:59:45 +0500

    add action list or profile

--- src/pages/ViewProfile.vue
+++ src/pages/ViewProfile.vue
@@ -1,60 +1,90 @@
 <template lang="pug">
   Layout
-    template(v-slot:content='' v-if="getOneProfile.profile")
-      v-layout(align-center='', justify-start='', row='', fill-height='')
-        .profile__main-info
-          v-avatar(
-            :tile="false"
-            size="64"
-            color="grey"
-          )
-            img(
-              :src="`https://joeschmoe.io/api/v1/${getOneProfile.profile.avatar}`"
-              alt="avatar"
-            )
-        v-layout(align-start='', justify-center='', column='', fill-height='')
-          h1.title.ml-4 {{ getOneProfile.profile.fullName }}
-          h2.subheading.ml-4 {{ getOneProfile.profile.birthday }} — {{year}}
-      .customSection__wrapper.mt-4
-        masonry(
-          :cols="{default: 3, 1000: 3, 768: 2, 400: 1}"
-          :gutter="{default: '30px', 768: '15px'}"
-        )
-          v-flex.customSection.item.box-shadow.mb-3(
-            v-for="(section, index) in getOneProfile.profile.sections"
-            :key='index'
-          )
-            // Header of section
-            v-layout(align-center='', justify-space-between='', row='', fill-height='').title__actions.pl-3.pt-2.pb-2
-              .title {{ section.sectionTitle }}
-              .title__actions
-                v-btn(fab, dark, outline, small, color='indigo', @click="section.showAddForm = !section.showAddForm")
-                  v-icon(dark='') add
-                v-btn(fab, dark, outline, small, color='teal')
-                  v-icon(dark='') edit
-            v-divider
-            // Print items
-            .section-items.pa-2(v-if="section.items.length")
-              .section-items__item
-                v-chip(color='blue', text-color='white',
-                v-for="(item, index) in section.items"
-                :key='index'
-              ) {{item.name}}
+    template(v-slot:content='' v-if="getOneProfile")
+      v-flex(xs12, align-center, justify-center='', layout='')
+        v-flex.text-xs-right(xs12, lg4, align-self-center)
+          v-chip(color='orange', text-color='white')
+            v-avatar.orange.darken-4 0
+            | Идей подарков
+          v-chip(color='indigo', text-color='white')
+            v-avatar.indigo.darken-4 8
+            | Увлечений
+        v-flex.text-xs-center(xs12, lg4)
+          v-avatar(tile, size='200')
+            img(:src='`https://joeschmoe.io/api/v1/${getOneProfile.profile.avatar}`', alt='avatar')
+        v-layout(align-start='', justify-space-between='', column='', fill-height='')
+          div.profile-main-info
+            h1.title {{ getOneProfile.profile.fullName }}
+              span.subheading.ml-3 21 год
+            v-flex.mt-4(xs12)
+              .caption Действия
+              v-tooltip(bottom='')
+                v-btn(flat='', icon='', slot='activator' color='pink')
+                  v-icon bookmark
+                span Добавить в закладки
+              v-tooltip(bottom='')
+                v-btn(flat='', icon='', slot='activator' color='indigo')
+                  v-icon share
+                span Поделиться
+              v-tooltip(bottom='')
+                v-btn(flat='', icon='', slot='activator' color='deep-orange')
+                  v-icon edit
+                span Редактировать профиль
 
-            // If nobody items
-            .section-items__empty.pa-4.text-xs-center(v-else)
-              span Пока тут пусто.
-              a(@click="section.showAddForm = !section.showAddForm")  Добавить
+      //- v-layout(align-center='', justify-start='', row='', fill-height='')
+      //-   .profile__main-info
+      //-     v-avatar(
+      //-       :tile="false"
+      //-       size="64"
+      //-       color="grey"
+      //-     )
+      //-       img(
+      //-         :src="`https://joeschmoe.io/api/v1/${getOneProfile.profile.avatar}`"
+      //-         alt="avatar"
+      //-       )
+      //-   v-layout(align-start='', justify-center='', column='', fill-height='')
+      //-     h1.title.ml-4 {{ getOneProfile.profile.fullName }}
+      //-     h2.subheading.ml-4 {{ getOneProfile.profile.birthday }} — {{year}}
+      //- .customSection__wrapper.mt-4
+      //-   masonry(
+      //-     :cols="{default: 3, 1000: 3, 768: 2, 400: 1}"
+      //-     :gutter="{default: '30px', 768: '15px'}"
+      //-   )
+      //-     v-flex.customSection.item.box-shadow.mb-3(
+      //-       v-for="(section, index) in getOneProfile.profile.sections"
+      //-       :key='index'
+      //-     )
+      //-       // Header of section
+      //-       v-layout(align-center='', justify-space-between='', row='', fill-height='').title__actions.pl-3.pt-2.pb-2
+      //-         .title {{ section.sectionTitle }}
+      //-         .title__actions
+      //-           v-btn(fab, dark, outline, small, color='indigo', @click="section.showAddForm = !section.showAddForm")
+      //-             v-icon(dark='') add
+      //-           v-btn(fab, dark, outline, small, color='teal')
+      //-             v-icon(dark='') edit
+      //-       v-divider
+      //-       // Print items
+      //-       .section-items.pa-2(v-if="section.items.length")
+      //-         .section-items__item
+      //-           v-chip(color='blue', text-color='white',
+      //-           v-for="(item, index) in section.items"
+      //-           :key='index'
+      //-         ) {{item.name}}
 
-            // Form for add item
-            .add-item-form(v-if="section.showAddForm")
-              v-divider
-              v-layout(align-center='', justify-center='', row='', fill-height='')
-                v-form(ref='form', lazy-validation='', @submit.prevent='')
-                  v-text-field(
-                    :label="`${section.sectionTitle}`"
-                  )
-                v-btn(small color="primary") Добавить
+      //-       // If nobody items
+      //-       .section-items__empty.pa-4.text-xs-center(v-else)
+      //-         span Пока тут пусто.
+      //-         a(@click="section.showAddForm = !section.showAddForm")  Добавить
+
+      //-       // Form for add item
+      //-       .add-item-form(v-if="section.showAddForm")
+      //-         v-divider
+      //-         v-layout(align-center='', justify-center='', row='', fill-height='')
+      //-           v-form(ref='form', lazy-validation='', @submit.prevent='')
+      //-             v-text-field(
+      //-               :label="`${section.sectionTitle}`"
+      //-             )
+      //-           v-btn(small color="primary") Добавить
 </template>
 
 <script>
@@ -79,10 +109,6 @@ export default {
   //     return titles[ (number % 100 > 4 && number % 100 < 20) ? 2 : this.cases[(number % 10 < 5) ? number % 10 : 5] ]
   //   }
   // },
-  name: 'Profile',
-  metaInfo: {
-    title: 'Profile'
-  },
   components: { Layout }
 }
 </script>
